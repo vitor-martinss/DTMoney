1f87bf4d0e2557a007508fb4a2a8cc42b799e559
Vitor Martins <vitor.martins@fever.pt>
2021-10-07 17:13:09 +0100

    UPD table and dynamic dates

--- src/DTMoney.Static/src/components/TransactionsTable/index.tsx
+++ src/DTMoney.Static/src/components/TransactionsTable/index.tsx
@@ -1,10 +1,22 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { api } from '../../services/api'
 import { Container } from './styles'
 
+interface Transaction {
+	id: number;
+	title: string;
+	amount: number;
+	type: string;
+	category: string;
+	createdAt: string;
+}
+
 export function TransactionsTable() {
+	const [transactions, setTransactions] = useState<Transaction[]>([])
+
 	useEffect(() => {
-		api.get('transactions').then(response => console.log(response.data))
+		api.get('/transactions')
+			.then(response => setTransactions(response.data['transactions']))
 	},[])
 
 	return (
@@ -19,24 +31,23 @@ export function TransactionsTable() {
 					</tr>
 				</thead>
 				<tbody>
-					<tr>
-						<td>Desenvolvimento de website</td>
-						<td className='deposit'>R$12.000,00</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2021</td>
-					</tr>
-					<tr>
-						<td>Desenvolvimento de website</td>
-						<td className='withdraw'>- R$12.000,00</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2021</td>
-					</tr>
-					<tr>
-						<td>Desenvolvimento de website</td>
-						<td>R$12.000,00</td>
-						<td>Desenvolvimento</td>
-						<td>20/02/2021</td>
-					</tr>
+					{transactions.map(transaction => (
+						<tr key={transaction.id}>
+							<td>{transaction.title}</td>
+							<td className={transaction.type}>
+								{
+									new Intl.NumberFormat('pt-BR', {
+										style: 'currency',
+										currency: 'BRL'
+									}).format(transaction.amount)
+								}
+							</td>
+							<td>{transaction.category}</td>
+							<td>
+								{new Intl.DateTimeFormat('pt-BR').format(new Date(transaction.createdAt))}
+							</td>
+						</tr>
+					))}
 				</tbody>
 			</table>
 		</Container>
